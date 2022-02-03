a80606496ab4a926e709f4d0f327a0df91655c9d
Drew Loveridge <drew.loveridge@makemarks.com>
2022-02-03 03:52:55 -0500

    House Cleaning

--- PiXYZ_Studio_Scripts/run_script.py
+++
@@ -1,4 +0,0 @@
-from PiXYZ_Studio_Scripts.demoBatch import processDirectory
-input_dir  = "U:/dloveridge/_User_Examples/bmerva/Whirlpool/QRefresh_Stl"
-output_dir = "U:/dloveridge/_User_Examples/bmerva/Whirlpool/QRefresh_Fbx"
-processDirectory(input_dir, ["stp"], output_dir, ["fbx"])
