17b109f6763948dbf776f9d3b9641bffba1fd911
Drew Loveridge <drew.loveridge@makemarks.com>
2022-02-03 03:58:12 -0500

    Clean up

--- PiXYZ_Studio_Scripts/AW_STD_Process.py
+++
@@ -1,5 +0,0 @@
-def runit():
-	all = [[scene.getRoot()]]
-	algo.tessellate(all, 0.050000, -1.000000, -10.000000, -1.000000, True, algo.UVGenerationMode.NoUV, False, False, False)
-	scene.deleteEmptyParts()
-runit()
